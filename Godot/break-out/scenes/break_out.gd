2996817fd61a31b9a96a29c3668300a11ffce328
Andy Pérez <andypr97vlc@gmail.com>
2025-12-06 19:17:21 +0100

    Proyecto Godot, Fase 2, en autoload

--- Godot/break-out/scenes/break_out.gd
+++ Godot/break-out/scenes/break_out.gd
@@ -6,24 +6,22 @@ var center
 func _ready() -> void:
 	var viewport_size=get_viewport().size
 	center = Vector2(viewport_size.x/2, viewport_size.y/2)
-	$Reset_Timer.timeout.connect(_on_reset_timer_timeout)
 	
+	# Inicializar la bola sobre el jugador
+	$Ball.reset_ball($Player)
 
-
-func reset():
-	if $Ball.visible:
-		$Ball.position = center
-		$Player.position.x = center.x
-		$Ball.visible = false
-		$Reset_Timer.start()
-
-
+# Señal que detecta cuando termina el Timer
 func _on_reset_timer_timeout():
 	$Ball.visible = true
-	$Ball.call("set_ball_velocity")
+	#$Ball.call("set_ball_velocity")
+	# Reset de la posicion del jugador y de la bola
+	$Player.position.x = center.x
+	$Ball.reset_ball($Player)
 
-
+# Si entra en el limite del juego, hara invisible la bola e inicia el Timer de 1s
 func _on_area_2d_game_limit_body_entered(body: Node2D) -> void:
 	if body.name != "Ball": return
-	reset()
+	
+	$Ball.visible = false
+	$Reset_Timer.start()
 	
